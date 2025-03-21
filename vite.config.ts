907fd67b00ec09da568f12888cd2b80e6880138e
Tuanxu5 <lehoangtuan012@gmail.com>
2025-03-21 14:39:39 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
-import path, { dirname } from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
+import react from "@vitejs/plugin-react";
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -30,7 +30,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+  outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
 });
