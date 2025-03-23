ad100a332bc8ad4d3cf8d9c7f45761d73288e3b3
Tuanxu5 <lehoangtuan012@gmail.com>
2025-03-23 20:45:43 +0700

    update

--- server/src/common/decorators/response.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-export const RESPONSE_MESSAGE = 'response_message';
-export const ResponseMessage = (message: string) =>
-  SetMetadata(RESPONSE_MESSAGE, message);
